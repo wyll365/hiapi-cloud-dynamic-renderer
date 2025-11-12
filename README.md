5d1f94b89b5135e12b20f30d4ce02462cae4d66a
adinz <adinz@adinz.com>
2025-11-12 23:36:01 +0800

    Init

---
+++ README.md
@@ -0,0 +1,9 @@
+# HIAPI-Cloud 项目 vite插件
+
+仅用于 HIAPI-Cloud uniapp前端项目
+
+发布: `npm publish --access public`
+
+npm config set registry https://registry.npmjs.org/
+
+npm config set registry https://registry.npmmirror.com/
---
+++ src/index.ts
@@ -0,0 +1,75 @@
+import fs from 'fs'
+import path from 'path'
+import chokidar from 'chokidar'
+
+import fg from 'fast-glob'
+
+type Options = {
+  dst: string | 'src/DynamicRenderer.vue'
+  dirs: string
+}
+const removeKeys = new Set(['src', 'components', 'index.vue'])
+
+function toKebabCase(name: string) {
+  return name.replace(/([a-z0-9])([A-Z])/g, '$1-$2').replace(/[_\s]+/g, '-').toLowerCase()
+}
+
+
+/**
+ * 自动生成 DynamicRenderer.vue
+ */
+export default async function dynamicRendererPlugin(options: Options) {
+  const {
+    dirs = 'src/components/**/*.vue',
+    dst = 'src/layouts/DynamicRenderer.vue',
+  } = options
+
+  const absOutputFile = path.resolve(process.cwd(), dst)
+  const modules = fg.sync(dirs)
+
+  function generateRenderer() {
+
+    const importLines = []
+    const renderLines = []
+    modules.forEach((path:string) => {
+      const parts = path.split('/').filter(p => p && !removeKeys.has(p)).map(e => e.toLowerCase())
+      const rawName = parts.join('-').replace('.vue', '')
+      const kebab = toKebabCase(rawName)
+      const pascal = rawName.split(/[-_]/)
+        .map(s => s.charAt(0).toUpperCase() + s.slice(1))
+        .join('')
+
+      importLines.push(`import ${pascal} from '${path}'`)
+      renderLines.push(`  <${kebab} v-if="(props.property?(item.component+'-property'):item.component) === '${kebab}'" :schema="item" />`)
+    })
+    const content = `
+<template>
+<!--兼容小程序  输出所有组建 -->
+<!--{{schemas}}-->
+  <view v-for="item in schemas">
+  ${renderLines.join('\n')}
+  </view>
+</template>
+
+<script setup lang="ts">
+import type {HiapiCloudSchemas,HiapiCloudSchema} from "@hiapi/hiapi-cloud-web-basic"
+const props = defineProps<{ layout: HiapiCloudSchemas,property:Boolean }>()
+const schemas:ComputedRef<Array<HiapiCloudSchema>> = computed(()=>props.layout?.schemas??[])
+</script>
+`
+    fs.writeFileSync(absOutputFile, content)
+    console.log(`✅ [DynamicRenderer] 已生成 (${modules.length} 个组件)`)
+  }
+
+  return {
+    name: 'vite-plugin-dynamic-renderer',
+    apply: 'serve', // 仅开发模式生效
+    buildStart() {
+      generateRenderer()
+      // 监听组件目录变化
+      const watcher = chokidar.watch(dirs, {ignoreInitial: true})
+      watcher.on('add', generateRenderer)
+      watcher.on('unlink', generateRenderer)
+    },
+  }
+}
